0aced3ec7c83140a1977790bdd76bec5aae502d1
PublicRyo <jdejesusrangel@unicesar.edu.co>
2025-02-23 09:39:28 -0500

    more pages

---
+++ scripts/choose.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+const button = document.getElementById("button 1")
+
+// Check if the elements are available
+if (button) {
+    console.log("Elements found. Setting up event listener...");
+	
+} else {
+    console.error("Elements not found.");
+}
--- scripts/home.js
+++ scripts/home.js
@@ -7,14 +7,22 @@
 const input = document.getElementById("username input")
 const button = document.getElementById("username submit")
 
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 // Check if the elements are available
 if (input && button) {
     console.log("Elements found. Setting up event listener...");
 
     // Add click event listener to the submit button
-    button.addEventListener("click", function() {
+    button.addEventListener("click", async function() {
         console.log("Button clicked!");
         input.value = "";  // Clear the input field
+		
+		console.log("Waiting!");
+		await delay(1000);
+		window.location.href = "choose.html";
     });
 } else {
     console.error("Elements not found.");
